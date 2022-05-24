1f76770641db03888f4b8241aab88acd72f2eebb
agile.zhou <kklldog@qq.com>
2022-05-24 14:21:24 +0800

    Update Program.cs

--- MysqldataTest/Program.cs
+++ MysqldataTest/Program.cs
@@ -3,8 +3,8 @@ using MysqldataTest;
 
 Console.WriteLine("Hello, World!");
 
-string connectString = "Database=agile;Data Source=r3nqzsdz9ism.ap-northeast-2.psdb.cloud;" +
-    "User Id=losuema80q51;Password=pscale_pw_MRKKdGy6mu9nUfyDoD_i8E8cyupV4iKNJA8p8qCn5S0;port=3306;" +
+string connectString = "Database=agile;Data Source=.;" +
+    "User Id=x;Password=xx;port=3306;" +
     "";
 
 var FREESQL = new FreeSql.FreeSqlBuilder()
